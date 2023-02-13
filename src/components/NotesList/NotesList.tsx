c5d8a33e4d0451ef8e461d772ce9fccf6f1856f2
Digambar <deshawaldigambar@gmail.com>
2023-02-13 20:59:05 +0530

    reafactor

--- src/components/NotesList/NotesList.tsx
+++ src/components/NotesList/NotesList.tsx
@@ -1,34 +1,38 @@
-import React from 'react'
-import { Card } from "../Card/Card"
+import React from "react";
+import { Card } from "../Card/Card";
+import "./NotesList.css";
 
-interface Note{
-    id:string,
-    title:string,
-    desc:string,
+interface Note {
+	id: string;
+	title: string;
+	desc: string;
+	color: string;
 }
 
-interface NotesProps{
-  notes:Note[],
-  setNotes:React.Dispatch<React.SetStateAction<Note[]>>
+interface NotesProps {
+	notes: Note[];
+	setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
 }
 
+const NotesList: React.FunctionComponent<NotesProps> = ({
+	notes,
+	setNotes,
+}) => {
+	const handleDelete = (id: string) => {
+		console.log(id);
+		setNotes(notes.filter((note) => note.id !== id));
+	};
 
-const NotesList:React.FunctionComponent<NotesProps>=({notes, setNotes})=>{
-
-  const handleDelete=(id:string)=>{
-    console.log(id)
-    setNotes(notes.filter(note=>note.id!==id))
-  }
-
-  return (
-    <>
-        {notes?.map(note=>
-        <>
-          <Card key={note.id} note={note} handleDelete={handleDelete}/>
-        </>)}
+	return (
+		<div className="card-container">
     
-    </>
-  )
-}
+			{notes.length ? notes.map((note) => (
+				<>
+					<Card key={note.id} note={note} handleDelete={handleDelete} />
+				</>
+			)) : <p className="empty-notes">You dont any notes, Try creating new notes</p>}
+		</div>
+	);
+};
 
-export  {NotesList}
+export { NotesList };
