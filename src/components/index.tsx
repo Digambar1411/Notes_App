77f6821c8f4d2a9f985abe3bdea65374f88f0744
Digambar <deshawaldigambar@gmail.com>
2023-02-13 21:00:56 +0530

    barrel import export

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 import { Card } from "./Card/Card";
 import { NotesList } from "./NotesList/NotesList";
+import { CreateNote} from "./CreateNote/CreateNote";
 
-export { Card, NotesList}
+export { Card, NotesList, CreateNote}
